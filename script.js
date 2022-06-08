c8e78fa7b020df2142638163df350634b228c926
Hasan <hasan.yilbat@gmail.com>
2022-06-08 19:20:09 +0300

    input box + fixed

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const toDoList = document.querySelector(".to-do-list");
 const toDone = document.querySelector(".to-done");
 const btn = document.querySelector(".btn");
 const input = document.querySelector("input");
+let input2 = input.value;
+
 window.addEventListener("load", () => {
   input.focus();
 });
@@ -18,15 +20,16 @@ toDoList.addEventListener("click", (e) => {
               <p>${input.value}</p>
               <div class="icon"><i class="fa-regular fa-square-check check"></i></div>
             </div>`;
+      input2 = input.value;
+      input.value = "";
     }
   }
   if (e.target.classList.contains("check")) {
     console.log(e.target.parentElement.parentElement);
     e.target.parentElement.parentElement.parentElement.nextElementSibling.innerHTML += ` <div class="par">
               <div class="del-icon icon ">
-               
               </div>
-              <p>${input.value}</p>
+              <p>${input2}</p>
               <div class="icon"> <i class="fa-solid fa-calendar-xmark del"></i></div>
             </div>`;
     e.target.parentElement.parentElement.remove();
